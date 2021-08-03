f907e4e8c7c9cf52f8dd03e260117cf564b50b72
CiroRiquelmee <ciroriquelme05@gmail.com>
2021-08-02 23:56:20 -0300

    first commit

---
+++ src/main/java/utn/isi/dan/eureka/DanEurekaApplication.java
@@ -0,0 +1,13 @@
+package utn.isi.dan.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/isi/dan/eureka/DanEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.isi.dan.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
